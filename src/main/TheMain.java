38d89c9f90cfca519c4a766c08285ab7cb9bdefa
David Wandratsch <david.wandratsch@htl-wels.at>
2021-02-23 21:41:43 +0100

    edited fxml-file in SceneBuilder

--- src/main/TheMain.java
+++ src/main/TheMain.java
@@ -11,7 +11,7 @@ public class TheMain extends Application
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("viewcontroller/view.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Phonebook");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
